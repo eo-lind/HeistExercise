1b924c3bab44d7fcff9764bf32226bfe8705e0f3
Olivia Lind <eo.lind@gmail.com>
2022-05-29 23:40:04 -0500

    1.1 complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+// static void Main(string[] args)
+// {
+// }
+    
+Console.WriteLine("Plan Your Heist!");
+
+namespace Heist
+{
+    // public TeamMember(string name, int skillLevel, double courageFactor)
+    // {
+    //     Name = name;
+    //     SkillLevel = skillLevel;
+    //     CourageFactor = courageFactor;
+    // }
+}
