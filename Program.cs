8e27edce8dffcbcdb44c23235d6be374ae8b71d8
Olivia Lind <eo.lind@gmail.com>
2022-05-30 00:07:28 -0500

    1.4 complete (will need to be changed later)

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ using System.Collections.Generic;
 Console.WriteLine("Plan Your Heist!");
 Console.WriteLine("Please provide the name of one of your team members.");
 string newMember = Console.ReadLine();
+// TODO: Save the skill level with the member (need to create a method for adding a team member)
+Console.WriteLine($"Please enter a numerical skill level for {newMember}.");
+string newMemberSkillLevelString = Console.ReadLine();
+int newMemberSkillLevel = int.Parse(newMemberSkillLevelString);
+Console.WriteLine($"{newMember}'s skill level is {newMemberSkillLevel}");
 
 // Create a way to store a single team member. A team member will have a name, a skill Level and a courage factor. The skill Level will be a positive integer and the courage factor will be a TODO: decimal between 0.0 and 2.0.
 namespace Heist
@@ -30,11 +35,6 @@ namespace Heist
     }
     // ----------------------------end team member class---------------------------------- //
 
-    
-
-
-
-    // TODO: Prompt the user to enter a team member's skill level and save that skill level with the name.
     // TODO: Prompt the user to enter a team member's courage factor and save that courage factor with the name.
     // TODO: Display the team member's information.
 }
