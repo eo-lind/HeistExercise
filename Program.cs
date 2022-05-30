abe405c8b9208f6080e62693867c4237fc1b3e33
Olivia Lind <eo.lind@gmail.com>
2022-05-29 23:48:22 -0500

    1.2 complete

--- Program.cs
+++ Program.cs
@@ -1,18 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 
-// static void Main(string[] args)
+//! static void Main(string[] args)
 // {
 // }
     
 Console.WriteLine("Plan Your Heist!");
 
+// Create a way to store a single team member. A team member will have a name, a skill Level and a courage factor. The skill Level will be a positive integer and the courage factor will be a TODO: decimal between 0.0 and 2.0.
 namespace Heist
 {
-    // public TeamMember(string name, int skillLevel, double courageFactor)
-    // {
-    //     Name = name;
-    //     SkillLevel = skillLevel;
-    //     CourageFactor = courageFactor;
-    // }
+    // ------------------------------team member class------------------------------------ //
+    public class TeamMember
+    {
+        // constructor for class properties
+        public string Name { get; set; }
+        public int SkillLevel { get; set ; }
+        public double CourageFactor { get; set; }
+
+        // constructor for instance of class (object)
+        public TeamMember(string name, int skillLevel, double courageFactor)
+        {
+            Name = name;
+            SkillLevel = skillLevel;
+            CourageFactor = courageFactor;
+        }
+    }
+    // ----------------------------end team member class---------------------------------- //
 }
